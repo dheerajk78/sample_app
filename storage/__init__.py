fde073f89362282883eea76abb1e735647653016
dheerajk78 <44789978+dheerajk78@users.noreply.github.com>
2025-08-31 16:48:39 +1000

    Update __init__.py

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,4 +1,13 @@
-from .gcs_backend import GCSStorageBackend
-from .firestore_backend import FirestoreStorageBackend
+from .config import get_backend_type
+from .gcs_backend import GCSBackend
+from .firestore_backend import FirestoreBackend
+import os
 
-__all__ = ["GCSStorageBackend", "FirestoreStorageBackend"]
+def get_storage_backend():
+    backend_type = get_backend_type()
+
+    if backend_type == "firestore":
+        return FirestoreBackend()
+    else:
+        bucket_name = os.environ.get("BUCKET_NAME", "your-bucket-name")
+        return GCSBackend(bucket_name)
