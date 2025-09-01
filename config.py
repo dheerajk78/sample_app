f01eecc5c46be7c8f71de1c138f923a2d2a08d67
dheerajk78 <44789978+dheerajk78@users.noreply.github.com>
2025-09-02 09:11:45 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "supersecretkey")
+    BUCKET_NAME = os.environ.get("BUCKET_NAME", "your-bucket-name")
+    CSV_FILENAME = "transactions.csv"
+    USERNAME = os.environ.get("UPLOAD_USER", "admin")
+    PASSWORD = os.environ.get("UPLOAD_PASS", "secret")
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=5)
