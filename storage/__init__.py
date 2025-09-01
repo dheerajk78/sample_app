9ca12bb4b650ceb95fafb3fba2edade16a798fe5
dheerajk78 <44789978+dheerajk78@users.noreply.github.com>
2025-09-01 14:20:11 +1000

    Update __init__.py

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,10 +1,10 @@
 from .gcs_backend import GCSBackend
 from .firestore_backend import FirestoreBackend
-from settings_manager import get_backend_toggle, set_backend_toggle
+from storage.config import get_backend_type, set_backend_type
 import os
 
 def get_storage_backend():
-    backend_type = get_backend_toggle()  # 🔄 use dynamic toggle from Firestore
+    backend_type = get_backend_type()  # 🔄 use dynamic toggle from Firestore
 
     if backend_type == "firestore":
         return FirestoreBackend()
