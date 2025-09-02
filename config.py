7a4130d3cad8b3917f075cd6b9511f408a2017e3
dheerajk78 <44789978+dheerajk78@users.noreply.github.com>
2025-09-02 12:16:37 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+import os
+
 class Config:
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "supersecretkey")
     BUCKET_NAME = os.environ.get("BUCKET_NAME", "your-bucket-name")
