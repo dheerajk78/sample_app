cea3c84fa597d446eef0d59089740bf937ea7a1a
dheerajk78 <44789978+dheerajk78@users.noreply.github.com>
2025-09-03 21:31:15 +1000

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -9,6 +9,7 @@ from typing import IO
 from io import StringIO
 from bs4 import BeautifulSoup
 from markupsafe import escape
+import html
 
 from utils import round2, percent, format_in_indian_system, parse_indian_value
 from storage import get_storage_backend
